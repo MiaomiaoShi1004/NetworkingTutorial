4d972b30f52325baceeb6d14b5fbd03523e56680
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-07-19 19:22:56 +0200

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Miaomiao Shi on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
